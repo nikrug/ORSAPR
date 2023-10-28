e20c23a8d5c7f714a921f1f292a19c268c8178ef
Никита Домрачев <domrachev-nikitka@mail.ru>
2023-10-28 12:54:39 +0700

    /feature создана верстка приложения отвертка

---
+++ Orsapr/Orsapr/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace Orsapr
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_Build = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_lenght_Of_bar = new System.Windows.Forms.TextBox();
+            this.textBox_widht_Of_bar = new System.Windows.Forms.TextBox();
+            this.textBox_widht_Of_tip = new System.Windows.Forms.TextBox();
+            this.textBox_widht_Of_handgrip = new System.Windows.Forms.TextBox();
+            this.textBox_heigt_Of_handgrip = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Длина стержня";
+            
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ширина стержня";
+            
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Размер наконечника";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ширина рукояти";
+            // 
+            // button_Build
+            // 
+            this.button_Build.Location = new System.Drawing.Point(160, 400);
+            this.button_Build.Name = "button_Build";
+            this.button_Build.Size = new System.Drawing.Size(75, 23);
+            this.button_Build.TabIndex = 4;
+            this.button_Build.Text = "Построить";
+            this.button_Build.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Высота рукояти";
+            // 
+            // textBox_lenght_Of_bar
+            // 
+            this.textBox_lenght_Of_bar.Location = new System.Drawing.Point(207, 36);
+            this.textBox_lenght_Of_bar.Name = "textBox_lenght_Of_bar";
+            this.textBox_lenght_Of_bar.Size = new System.Drawing.Size(100, 23);
+            this.textBox_lenght_Of_bar.TabIndex = 6;
+            
+            
+           
+            // 
+            // textBox_widht_Of_bar
+            // 
+            this.textBox_widht_Of_bar.Location = new System.Drawing.Point(207, 84);
+            this.textBox_widht_Of_bar.Name = "textBox_widht_Of_bar";
+            this.textBox_widht_Of_bar.Size = new System.Drawing.Size(100, 23);
+            this.textBox_widht_Of_bar.TabIndex = 7;
+            // 
+            // textBox_widht_Of_tip
+            // 
+            this.textBox_widht_Of_tip.Location = new System.Drawing.Point(207, 131);
+            this.textBox_widht_Of_tip.Name = "textBox_widht_Of_tip";
+            this.textBox_widht_Of_tip.Size = new System.Drawing.Size(100, 23);
+            this.textBox_widht_Of_tip.TabIndex = 8;
+            // 
+            // textBox_widht_Of_handgrip
+            // 
+            this.textBox_widht_Of_handgrip.Location = new System.Drawing.Point(207, 180);
+            this.textBox_widht_Of_handgrip.Name = "textBox_widht_Of_handgrip";
+            this.textBox_widht_Of_handgrip.Size = new System.Drawing.Size(100, 23);
+            this.textBox_widht_Of_handgrip.TabIndex = 9;
+            // 
+            // textBox_heigt_Of_handgrip
+            // 
+            this.textBox_heigt_Of_handgrip.Location = new System.Drawing.Point(207, 227);
+            this.textBox_heigt_Of_handgrip.Name = "textBox_heigt_Of_handgrip";
+            this.textBox_heigt_Of_handgrip.Size = new System.Drawing.Size(100, 23);
+            this.textBox_heigt_Of_handgrip.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(332, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "20-200мм";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(332, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 15);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "1-10мм";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(332, 139);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 15);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "1-10мм";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(332, 187);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 15);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "5-40мм";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(332, 235);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 15);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "5-40мм";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_heigt_Of_handgrip);
+            this.Controls.Add(this.textBox_widht_Of_handgrip);
+            this.Controls.Add(this.textBox_widht_Of_tip);
+            this.Controls.Add(this.textBox_widht_Of_bar);
+            this.Controls.Add(this.textBox_lenght_Of_bar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button_Build);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Отвертка";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_Build;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_lenght_Of_bar;
+        private System.Windows.Forms.TextBox textBox_widht_Of_bar;
+        private System.Windows.Forms.TextBox textBox_widht_Of_tip;
+        private System.Windows.Forms.TextBox textBox_widht_Of_handgrip;
+        private System.Windows.Forms.TextBox textBox_heigt_Of_handgrip;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
